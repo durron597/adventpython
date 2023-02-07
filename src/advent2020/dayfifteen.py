45e587cdfaa04a0f5e2b221d1eaa88eb9eb6b5de
durron597 <martin.jared@gmail.com>
2023-02-06 20:19:19 -0800

    Day 15.
    Part two is pretty slow, but not so slow to be intolerable. More than 2 minutes, but less than 10.

---
+++ src/advent2020/dayfifteen.py
@@ -0,0 +1,47 @@
+import re
+from collections import deque
+
+
+def part_one(str_list):
+    return [play_game(i.strip(), 2020) for i in str_list]
+
+
+def say(history, turn, value_to_say):
+    old_val = history.get(value_to_say, deque([], 2))
+    old_val.append(turn)
+    history.update({value_to_say: old_val})
+
+    return value_to_say
+
+
+def play_game(start_str, stop):
+    seed = [int(x) for x in re.split(",", start_str)]
+    history = {}
+    last = -1
+
+    for i in range(stop):
+        if i < len(seed):
+            last = say(history, i, seed[i])
+        else:
+            q = history[last]
+            v = q.popleft()
+            if q:
+                v2 = q.popleft()
+                q.append(v)
+                q.append(v2)
+                last = say(history, i, v2 - v)
+            else:
+                q.append(v)
+                last = say(history, i, 0)
+
+    return last
+
+
+def part_two(str_list):
+    ret = []
+    for i in str_list:
+        result = play_game(i.strip(), 30000000)
+        print(result)
+        ret.append(result)
+
+    return ret
